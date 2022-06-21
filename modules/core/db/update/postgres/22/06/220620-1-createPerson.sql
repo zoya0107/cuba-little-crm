4eccae316da4bd76aa864822875b2232a5fd9222
Zoya Nazarova <zoyavishni@gmail.com>
2022-06-21 15:28:30 +0700

    sql: db scripts

---
+++ modules/core/db/update/postgres/22/06/220620-1-createPerson.sql
@@ -0,0 +1,16 @@
+create table CUBALITTLECRM_PERSON (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    FIRST_NAME varchar(255) not null,
+    LAST_NAME varchar(255) not null,
+    LOGIN varchar(255) not null,
+    --
+    primary key (ID)
+);
